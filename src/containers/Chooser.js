3d817ca6f4b69b0b2e6c5da2d2fcd6fa743eef2d
Becky McCarthy <beckymccarthy777@gmail.com>
2020-02-17 17:51:06 -0800

    fixed css on cards

--- src/containers/Chooser.js
+++ src/containers/Chooser.js
@@ -13,14 +13,23 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 
 const Chooser = props => {
-  // const myStyle = {
-  //   border: "1px Solid Gray",
-  //   listStyleType: "none",
-  //   boxShadow: "2px 2px grey",
-  //   marginBottom: "2em",
-  //   padding: "1em",
-  //   lineHeight: "1.5em"
-  // }
+  const cardStyle = {
+    border: "1px Solid Gray",
+    borderRadius: "0.5em",
+    listStyleType: "none",
+    boxShadow: "2px 2px grey",
+    marginBottom: "2em",
+    padding: "1em",
+    lineHeight: "1.5em",
+    maxWidth: "11em"
+  }
+
+  const imgStyle = {
+    height: "8em",
+    width: "8em",
+
+  }
+
   return (
 
     <div>
@@ -28,35 +37,33 @@ const Chooser = props => {
       <Grid
         container
         direction="row"
-        justify-content="space-evenly"
-        alignItems="center"
+        justify="center"
+      // alignItems="baseline"
       >
 
         {categories.map(category => {
           return (
 
-            <Grid item xs={6} sm={4}>
+            <Grid item xs>
 
               <Link to={`/ailment/${category.id}`}
                 key={category.id}
               >
-                <Card>
-                  <CardContent style={{ justifyContent: "center" }}>
-                    {/* <CardActions > */}
+                {/* <Card>
+                  <CardContent> */}
+                {/* <CardActions > */}
 
 
-                    <div
-                    // style={myStyle}
-                    >
-                      <h2 >{category.title}</h2>
-                      <img src={category.image} alt={category.imageAltText} />
-                      <h5>{category.description}</h5>
-                    </div>
+                <div style={cardStyle}>
+                  <h2 >{category.title}</h2>
+                  <img src={category.image} alt={category.imageAltText} style={imgStyle} />
+                  <h5>{category.description}</h5>
+                </div>
 
-                    {/* </CardActions> */}
-                  </CardContent>
+                {/* </CardActions> */}
+                {/* </CardContent>
 
-                </Card>
+                </Card> */}
               </Link>
             </Grid>
           )
@@ -64,7 +71,11 @@ const Chooser = props => {
         })
         }
       </Grid>
+      <>
+        <h1>More helpful information</h1>
+        <h3><a href="https://stantonmigraineprotocol.com/">Magraine Help</a></h3>
 
+      </>
       {/* </ul> */}
 
       {/*<Card>
