0b361a16cfb9de3122cc4476bd547d15005cba56
Becky McCarthy <beckymccarthy777@gmail.com>
2020-02-25 16:27:48 -0800

    looded food category chooser to sanity

--- src/containers/FoodChooser.js
+++ src/containers/FoodChooser.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import foodCats from "../mock/foodCategories";
@@ -6,25 +6,52 @@ import { Link } from "react-router-dom";
 import { increment, decrement } from "../store/reducers/stepCounter";
 import Grid from '@material-ui/core/Grid';
 import 'typeface-roboto';
+import client from '../client';
+import myConfigSanityClient from '../client';
+import imageUrlBuilder from '@sanity/image-url';
 
-const FoodChooser = props => {
-    const cardStyle = {
-        border: "1px Solid Gray",
-        borderRadius: "0.5em",
-        listStyleType: "none",
-        boxShadow: "2px 2px grey",
-        marginBottom: "2em",
-        padding: "1em",
-        lineHeight: "1.5em",
-        maxWidth: "12em",
-        minWidth: "12em",
+const builder = imageUrlBuilder(myConfigSanityClient);
+
+const cardStyle = {
+    border: "1px Solid Gray",
+    borderRadius: "0.5em",
+    listStyleType: "none",
+    boxShadow: "2px 2px grey",
+    marginBottom: "2em",
+    padding: "1em",
+    lineHeight: "1.5em",
+    maxWidth: "12em",
+    minWidth: "12em",
+}
+
+const imgStyle = {
+    height: "8em",
+    width: "8em",
+
+}
+
+function FoodChooser(props) {
+
+    const [foodCat, setFoodCat] = useState([]);
+
+    useEffect(() => {
+        onLoad()
+    }, [])
+    async function onLoad() {
+        try {
+            const foodGroups = await client.fetch(`
+                *[_type == 'categories-foods']{
+                    title, slug, image, imageAltText}`)
+            console.log("foodCat testing: ", foodGroups);
+            setFoodCat(foodGroups);
+        } catch (e) {
+            if (e !== "No current user") {
+                alert(e)
+            }
+        }
+        // setIsLoading(false);
     }
 
-    const imgStyle = {
-        height: "8em",
-        width: "8em",
-
-    }
 
     return (
         <div>
@@ -33,16 +60,18 @@ const FoodChooser = props => {
                 direction="row"
                 justify="center"
             >
-                {foodCats.map(foodCat => {
-                    console.log(foodCat);
+                {foodCat.map((foodCat, index) => {
+                    function urlFor(_ref) {
+                        return builder.image(_ref)
+                    }
                     return (
                         <Grid item xs>
-                            <Link to={`/foods/category/${foodCat.name}`}
+                            <Link to={`/foods/category/${foodCat.slug.current}`}
                                 key={foodCat.id}
                             >
                                 <div style={cardStyle}>
                                     <h2 style={{ padding: "0.5em" }}>{foodCat.title}</h2>
-                                    <img src={foodCat.image} alt={foodCat.imageAltText} style={imgStyle} />
+                                    <img src={urlFor(foodCat.image.asset._ref)} alt={foodCat.imageAltText} style={imgStyle} />
                                     <h5>{foodCat.description}</h5>
                                 </div>
                             </Link>
