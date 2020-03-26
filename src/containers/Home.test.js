a3d165ddc6a544ed4f99125f9d1f7cfe9efc61a4
Becky McCarthy <beckymccarthy777@gmail.com>
2020-03-26 07:30:11 -0700

    setup <Home/> and install "@testing-library/react"

---
+++ src/containers/Home.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Home from './Home';
+
+describe('this is a test', () => {
+    // this is where your tests go
+    const setUp = render(<Home />)
+    it('this is an individual test', () => {
+        // this is the code for our individual test
+        // such as make sure the "Choose an ailment..." button displays
+
+    })
+} ) 
