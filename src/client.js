7450569aeba4c92245cc79d9c224c78e8c6dc749
Becky McCarthy <beckymccarthy777@gmail.com>
2020-04-11 09:59:39 -0700

    client fix

--- src/client.js
+++ src/client.js
@@ -3,5 +3,5 @@ import sanityClient from "@sanity/client"
 export default sanityClient({
     projectId: "isuhgmk8", // you can find this in sanity.json
     dataset: "production", // or th name you chose in step 1
-    useCdn: true, // `false` if you want to ensure fresh data
+    useCdn: false, // `false` if you want to ensure fresh data
 })
