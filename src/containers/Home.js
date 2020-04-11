355c251324502a2e1b20eb610621f65daca1c850
Becky McCarthy <beckymccarthy777@gmail.com>
2020-04-11 10:05:28 -0700

    home cleaned up

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,9 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import CardActions from "@material-ui/core/CardActions";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from '@material-ui/core/styles';
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
-// import categories from "../mock/categories";
 import { Link } from "react-router-dom";
 import { increment, decrement } from "../store/reducers/stepCounter";
 import video from "../assets/video.mp4";
