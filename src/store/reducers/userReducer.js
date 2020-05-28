68c3088f0832b742273720569ea652fcc2917f7f
Becky McCarthy <beckymccarthy777@gmail.com>
2020-05-28 11:27:48 -0700

    cleanup

--- src/store/reducers/userReducer.js
+++ src/store/reducers/userReducer.js
@@ -1,7 +1,6 @@
 export default function userReducer(state = [], action) {
     switch (action.type) {
         case "LOAD_USER":
-            console.log("action object", action)
             return [...action.ailments];
         default:
             return state;
