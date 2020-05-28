db517be2d57fbe49f42b1dbf2f0a82101afddf6a
Becky McCarthy <beckymccarthy777@gmail.com>
2020-05-28 11:28:08 -0700

    cleanup

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import registerServiceWorker from "./registerServiceWorker";
-
 import App from "./App";
 import store from "./store/configureStore";
-console.log("store: ", store);
 
 let data = store.getState();
-console.log("data from getState: ", data);
 ReactDOM.render(
   <Provider store={store}>
     <App />
