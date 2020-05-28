b46926808a7f6d5bfb3adb16ff8cc52ba11ebc84
Becky McCarthy <beckymccarthy777@gmail.com>
2020-05-28 09:54:28 -0700

    more cleanup

--- src/containers/Nutrient.js
+++ src/containers/Nutrient.js
@@ -47,7 +47,6 @@ function urlFor(_ref) {
 function Nutrients(props) {
     const classes = useStyles();
     const { nutrient } = props
-    console.log("nutrient", nutrient)
     if (!nutrient) {
         return <div>Nutrient doesn't exist</div>
     } else {
@@ -57,50 +56,24 @@ function Nutrients(props) {
                     <CardContent>
                         <Typography className={classes.title}>{nutrient.title}</Typography>
                         <img src={urlFor(nutrient.mainImage.asset._ref)} alt={nutrient.imageAltText} style={imgStyle} />
-                        <Typography className={classes.pos} variant="h6">Possible Benefits</Typography>
 
+                        <Typography className={classes.pos} variant="h6">Possible Benefits</Typography>
                         <Typography className={classes.top} variant="h6">
                             {nutrient.body[0].children[0].text}
                         </Typography>
+
                         <Typography className={classes.top} variant="h6">Helpful foods</Typography>
                         {nutrient.ingredients.map((food, i) => {
-
                             console.log(food)
                             return (
-
-
-                                // <Link to={`/ingredient/${food}`}
-                                //     key={food.id}
-                                // >
                                 <li key={i}>
 
                                     {food}
                                 </li>
-                                // </Link>
                             )
                         })}
-
                     </CardContent>
                 </Card>
-                {/* <Grid
-                container
-                direction="row"
-                justify="center"
-            >
-                {props.everything.nutrients.map((nutrient, i) => {
-                    {nutriSingle.map((nutrient, index) => { 
-                    console.log(nutrient);
-                    if (nutrient.slug.current === nutrientChose) {
-
-                        return (
-                            < Grid item xs >
-                            </Grid>
-
-
-                        )
-                    }
-                })}
-            </Grid> */}
             </div >
         )
     }
