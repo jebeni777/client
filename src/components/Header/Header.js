0218b65a54a0098337c6cddb205801fc43994c8f
Becky McCarthy <beckymccarthy777@gmail.com>
2020-06-09 17:29:36 -0700

    changed nav, moved tiny link and added css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,14 +4,20 @@ import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Badge from "@material-ui/core/Badge";
 import MenuIcon from "@material-ui/icons/Menu";
-import NotificationsIcon from "@material-ui/icons/Notifications";
+import SvgIcon from '@material-ui/core/SvgIcon';
+import teal from '@material-ui/core/colors/teal'
+// import NotificationsIcon from "@material-ui/icons/Notifications";
 import PersonIcon from "@material-ui/icons/Person";
 import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
 
-
 const styles = theme => ({
+  root: {
+    '& > svg': {
+      margin: theme.spacing(2),
+    },
+  },
   toolbarRoot: {
     paddingRight: 24
   },
@@ -20,24 +26,37 @@ const styles = theme => ({
     marginRight: 36
   },
   title: {
-    flexGrow: 1
+    flexGrow: 1,
+    marginLeft: 110
   }
-
 });
+
+const imgStyle = {
+  height: "1.5em",
+  weight: "1.5em",
+};
+
+function HomeIcon(props) {
+  return (
+    <SvgIcon {...props}>
+      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+    </SvgIcon>
+  );
+}
 
 const Header = props => {
   const { classes, handleToggleDrawer } = props;
   return (
     <AppBar position="fixed">
       <Toolbar disableGutters={true} classes={{ root: classes.toolbarRoot }}>
-        <IconButton
+        {/* <IconButton
           color="inherit"
           aria-label="Open drawer"
           onClick={handleToggleDrawer}
           className={classes.menuButton}
         >
           <MenuIcon />
-        </IconButton>
+        </IconButton> */}
         <Typography
           variant="body1"
           component="h1"
@@ -47,11 +66,16 @@ const Header = props => {
         >
           Comfort Foods
         </Typography>
-        <IconButton color="inherit">
+        <Link to="/">
+          <IconButton color="inherit">
+            <HomeIcon style={{ color: teal[50] }} />
+          </IconButton>
+        </Link>
+        {/* <IconButton color="inherit">
           <Badge badgeContent={4} color="secondary">
             <NotificationsIcon />
           </Badge>
-        </IconButton>
+        </IconButton> */}
         <Link to="/user">
           <IconButton color="inherit">
             <PersonIcon />
